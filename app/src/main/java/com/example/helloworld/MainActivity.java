229d0c298b91c6bf71b44ac613b96493f4cf3cec
neethu3101 <neethuliz2001@gmail.com>
2022-06-19 23:46:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtHello = findViewById(R.id.helloWorld);
+        txtHello.setText("Hello");
+    }
+
+
+}
